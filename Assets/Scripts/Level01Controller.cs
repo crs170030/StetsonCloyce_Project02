480642fe6a8eb045a0392b4658d0e151d6788681
Cloyce Stetson <crs170030@utdallas.edu>
2020-09-24 17:19:43 -0500

    Followed Setup Document
    Added Main Menu UI, music, and highscore system

---
+++ Assets/Scripts/Level01Controller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Level01Controller : MonoBehaviour
+{
+    [SerializeField] Text _currentScoreTextView;
+
+    int _currentScore;
+    string highScoreSt = "HighScore";
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //Increase Score
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            IncreaseScore(5);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitLevel();
+        }
+    }
+
+    public void IncreaseScore(int scoreIncrease)
+    {
+        //increase score
+        _currentScore += scoreIncrease;
+        //update score display so we can see new score
+        _currentScoreTextView.text = "Score: " + _currentScore.ToString();
+    }
+
+    public void ExitLevel()
+    {
+        //check if score is greater than highscore
+        int highScore = PlayerPrefs.GetInt(highScoreSt);
+        if(_currentScore > highScore)
+        {
+            //save current score as new high score
+            PlayerPrefs.SetInt(highScoreSt, _currentScore);
+            Debug.Log("New high score: " + _currentScore);
+        }
+        //load level
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] AudioClip _startingSong;
+    [SerializeField] Text _highScoreTextView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //load highscore to display
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        _highScoreTextView.text = highScore.ToString();
+
+        //play song on start
+        if(_startingSong != null){
+            AudioManager.Instance.PlaySong(_startingSong);
+        }
+    }
+}
